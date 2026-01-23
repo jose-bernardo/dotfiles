325721cec447566dd95b56fc3ecd35aa9e76e613
larry <>
2026-01-23 21:47:30 +0000

    bfline

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -55,16 +55,16 @@ return {
     init = function()
       vim.cmd.colorscheme("catppuccin-mocha")
     end,
-    specs = {
-      {
-        "akinsho/bufferline.nvim",
-        version = "*",
-        opts = function(_, opts)
-          if (vim.g.colors_name or ""):find("catppuccin") then
-            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
-          end
-        end,
-      },
-    },
+    -- specs = {
+    --   {
+    --     "akinsho/bufferline.nvim",
+    --     version = "*",
+    --     opts = function(_, opts)
+    --       if (vim.g.colors_name or ""):find("catppuccin") then
+    --         opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+    --       end
+    --     end,
+    --   },
+    -- },
   },
 }
